25f25521149a48addbd75b7f4fafc5ac0e60841b
Jim Chen <jimchen4214@gmail.com>
2025-02-10 16:34:06 +0800

    Updates

--- popup.js
+++ popup.js
@@ -1,3 +1,21 @@
+async function getStor(key) {
+  return new Promise((resolve) => {
+      if (!key)
+          chrome.storage.local.get(null, resolve);
+      else
+          chrome.storage.local.get([key], (result) => resolve(result[key]));
+  });
+}
+
+async function setStor(key, val) {
+  return new Promise((resolve) => {
+      var obj = {};
+      obj[key] = val;
+      chrome.storage.local.set(obj, resolve);
+  });
+}
+
+
 var onrd = new Array();
 document.addEventListener("DOMContentLoaded", (event) => {
   for (var i = 0; i < onrd.length; ++i) {
